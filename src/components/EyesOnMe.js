21670b8f29bc1e40595a3c6579b7f533d3c2da73
barangievans <barangievans@gmail.com>
2024-08-19 02:33:45 +0300

    lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,25 @@
-// Code EyesOnMe Component Here
+// components/EyesOnMe.js
+import React from 'react';
+
+const EyesOnMe = () => {
+  // Handler for when the button gains focus
+  const handleFocus = () => {
+    console.log('Good!');
+  };
+
+  // Handler for when the button loses focus
+  const handleBlur = () => {
+    console.log('Hey! Eyes on me!');
+  };
+
+  return (
+    <button
+      onFocus={handleFocus} // Attach the focus event handler
+      onBlur={handleBlur}   // Attach the blur event handler
+    >
+      Eyes on me
+    </button>
+  );
+};
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,18 @@
-// Code Keypad Component Here
+// components/Keypad.js
+import React from 'react';
 
-function Keypad (){
-    return (
-        <div></div>
-    )
-}
+const Keypad = () => {
+  // Handler for when the input value changes
+  const handleChange = (event) => {
+    console.log('Entering password...');
+  };
+
+  return (
+    <input
+      type="password"     // Set input type to password
+      onChange={handleChange} // Attach the change event handler
+    />
+  );
+};
 
 export default Keypad;
